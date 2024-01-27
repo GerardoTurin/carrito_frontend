55f619d4fb9f9ece18d98c5050df6debd2323542
Gerardo Turin <gera_1993_06@hotmail.com>
2024-01-27 19:18:24 -0300

    Carrito front 8 commit

--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -5,7 +5,7 @@ export const getEnvVariables = () => {
 
     return {
         VITE_MODE: import.meta.env.VITE_MODE,
-        VITE_API_URL: import.meta.env.VITE_API_URL,
+        VITE_API_URL: import.meta.env.VITE_API_BACK_URL,
         //...import.meta.env,
     };
 };
